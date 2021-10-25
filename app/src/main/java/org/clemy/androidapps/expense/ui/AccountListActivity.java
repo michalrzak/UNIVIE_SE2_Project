cc065ceb964d0f6fa6a2c9cfa4557a8fc1cb6308
Bernhard C. Schrenk <bernhard.clemens.schrenk@univie.ac.at>
2021-10-26 01:20:53 +0200

    refactor

--- app/src/main/java/org/clemy/androidapps/expense/ui/AccountListActivity.java
+++ app/src/main/java/org/clemy/androidapps/expense/ui/AccountListActivity.java
@@ -1,19 +1,17 @@
 package org.clemy.androidapps.expense.ui;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
-import android.util.Log;
 
 import org.clemy.androidapps.expense.R;
 import org.clemy.androidapps.expense.database.Repository;
 import org.clemy.androidapps.expense.model.Account;
 import org.clemy.androidapps.expense.model.AccountList;
 import org.clemy.androidapps.expense.model.AccountType;
-import org.clemy.androidapps.expense.utils.AppContextStore;
-import org.clemy.androidapps.expense.utils.ChangingData;
 import org.clemy.androidapps.expense.utils.ChangingDataWithLifecycle;
 
 import java.util.Timer;
@@ -35,27 +33,13 @@ public class AccountListActivity extends AppCompatActivity {
 
         final ChangingDataWithLifecycle<AccountList> accountsData =
                 new ChangingDataWithLifecycle<>(repository.getAccounts(), getLifecycle());
-        accountsData.observe(new ChangingData.Observer<AccountList>() {
-            @Override
-            public void changed(AccountList data) {
-                for (Account account: data.getAccountList()) {
-                    Log.d(TAG, "account " + account.getId());
-                }
-                Log.d(TAG, "changed " + data.getAccountList().size());
-                adapter.submitList(data.getAccountList());
-            }
-        });
+        accountsData.observe(data -> adapter.submitList(data.getAccountList()));
         final Timer timer = new Timer(true);
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
                 Log.d(TAG, "Timer");
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        repository.addAccount(new Account("newNew", AccountType.BANK));
-                    }
-                });
+                repository.addAccount(new Account("newNew", AccountType.BANK));
                 //timer.cancel();
             }
         }, 3000, 3000);
