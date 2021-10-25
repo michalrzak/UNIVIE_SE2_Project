39290b913cd6287309af1dbc6f63942b7210c5ea
Bernhard C. Schrenk <bernhard.clemens.schrenk@univie.ac.at>
2021-10-26 01:07:03 +0200

    refactor

--- app/src/main/java/org/clemy/androidapps/expense/database/MemoryDb.java
+++ app/src/main/java/org/clemy/androidapps/expense/database/MemoryDb.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 
 import org.clemy.androidapps.expense.model.Account;
 import org.clemy.androidapps.expense.model.AccountList;
-import org.clemy.androidapps.expense.model.AccountType;
 
 import java.util.ArrayList;
 import java.util.List;
