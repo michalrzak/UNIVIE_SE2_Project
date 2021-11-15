4ef46a2a0bc9280697e754d086a05d862c7418e2
Rumen Angelov <a11911470@unet.univie.ac.at>
2021-11-15 15:46:06 +0100

    changed recyclerview.adapter to listadapter to allow for automatic list updates

--- implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/model/account/AppAccount.java
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/model/account/AppAccount.java
@@ -38,4 +38,12 @@ public class AppAccount {
     public int hashCode() {
         return name.hashCode();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AppAccount that = (AppAccount) o;
+        return type == that.type && name.equals(that.name);
+    }
 }
--- implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/ui/mainactivity/MainActivity.java
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/ui/mainactivity/MainActivity.java
@@ -2,9 +2,11 @@ package at.ac.univie.se2.ws21.team0404.app.ui.mainactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import at.ac.univie.se2.ws21.team0404.app.R;
+import at.ac.univie.se2.ws21.team0404.app.ui.accountList.AccountList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // temporary for testing
+        Intent intent = new Intent(this, AccountList.class);
+        startActivity(intent);
     }
 }
