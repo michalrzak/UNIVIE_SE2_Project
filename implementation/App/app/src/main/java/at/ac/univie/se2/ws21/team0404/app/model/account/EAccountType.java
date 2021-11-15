39c9404fc6068b42061c39d9319a4c76cf138ffb
Michal Robert Zak <a11922222@unet.univie.ac.at>
2021-11-15 11:08:27 +0100

    started work on account implementation

---
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/model/account/EAccountType.java
@@ -0,0 +1,11 @@
+package at.ac.univie.se2.ws21.team0404.app.model.account;
+
+/**
+ * An Enum specifying the different account types available to the user.
+ * May be replaced later with something more complex, which allows to add accounts runtime.
+ */
+
+
+public enum EAccountType {
+    CASH, BANK, Card, Stock
+}
