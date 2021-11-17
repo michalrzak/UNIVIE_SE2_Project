ffcaaae0f697470235e77c0229ca61f417d9e3ae
Bernhard Schrenk <clemy@clemy.org>
2021-11-18 00:21:34 +0100

    own @NonNull and @Nullable annotations including Android Studio configuration

--- implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/MemoryDatabase.java
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/MemoryDatabase.java
@@ -2,14 +2,13 @@ package at.ac.univie.se2.ws21.team0404.app.database;
 
 import android.provider.ContactsContract;
 
-import androidx.annotation.NonNull;
-
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 import at.ac.univie.se2.ws21.team0404.app.model.account.AppAccount;
 import at.ac.univie.se2.ws21.team0404.app.utils.exceptions.DataExistsException;
+import at.ac.univie.se2.ws21.team0404.app.utils.NonNull;
 
 public class MemoryDatabase implements IDatabase{
 
--- implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/utils/ChangingDataOnMainThread.java
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/utils/ChangingDataOnMainThread.java
@@ -1,7 +1,5 @@
 package at.ac.univie.se2.ws21.team0404.app.utils;
 
-import androidx.annotation.NonNull;
-
 import java.util.HashMap;
 import java.util.Map;
 
