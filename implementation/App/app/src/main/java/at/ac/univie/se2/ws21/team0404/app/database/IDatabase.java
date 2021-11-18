dbc3da7ee35d3ab034026b30aac8a5641c1a1823
Michal Robert Zak <a11922222@unet.univie.ac.at>
2021-11-18 15:21:04 +0100

    Expanded database to be able to get/add transactions of/to a specific account

--- implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/IDatabase.java
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/IDatabase.java
@@ -1,18 +1,31 @@
 package at.ac.univie.se2.ws21.team0404.app.database;
 
+import android.provider.ContactsContract;
+
+import androidx.annotation.NonNull;
+
 import java.util.Collection;
 
 import at.ac.univie.se2.ws21.team0404.app.model.account.AppAccount;
+import at.ac.univie.se2.ws21.team0404.app.model.transaction.Transaction;
 import at.ac.univie.se2.ws21.team0404.app.utils.exceptions.DataDoesNotExistException;
 import at.ac.univie.se2.ws21.team0404.app.utils.exceptions.DataExistsException;
 
 /**
- * Interface, used for our databse implementation. Allows us to have a test database
+ * Interface, used for our database implementation. Allows us to have a test database
  * that does not rely on Room
  */
 public interface IDatabase {
     Collection<AppAccount> getAccounts();
-    void addAccount(AppAccount newAccount) throws DataExistsException;
+    Collection<Transaction> getTransactions(@NonNull AppAccount account) throws DataDoesNotExistException;
+
+    void addAccount(@NonNull AppAccount newAccount) throws DataExistsException;
+    void addTransaction(@NonNull AppAccount owner, @NonNull Transaction newTransaction)
+            throws DataExistsException, DataDoesNotExistException;
+    // the transaction with the given ID can already exist or the owner of the transaction may
+    // not exist; may get changed once actual DB implementation finalises and it becomes clear
+    // how transaction ids will be handled.
+
     // should not be needed
     // public void updateAccount(AppAccount oldAccount, AppAccount newAccount) throws DataDoesNotExistException;
 }
--- implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/MemoryDatabase.java
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/MemoryDatabase.java
@@ -1,18 +1,23 @@
 package at.ac.univie.se2.ws21.team0404.app.database;
 
-import android.provider.ContactsContract;
+import androidx.annotation.NonNull;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import at.ac.univie.se2.ws21.team0404.app.model.account.AppAccount;
+import at.ac.univie.se2.ws21.team0404.app.model.transaction.Transaction;
+import at.ac.univie.se2.ws21.team0404.app.utils.exceptions.DataDoesNotExistException;
 import at.ac.univie.se2.ws21.team0404.app.utils.exceptions.DataExistsException;
 import at.ac.univie.se2.ws21.team0404.app.utils.NonNull;
 
 public class MemoryDatabase implements IDatabase{
 
-    private Set<AppAccount> accounts = new HashSet<>();
+    private final Set<AppAccount> accounts = new HashSet<>();
+    private final Map<AppAccount, Set<Transaction>> transactions = new HashMap<>();
 
     @Override
     public Collection<AppAccount> getAccounts(){
@@ -20,9 +25,29 @@ public class MemoryDatabase implements IDatabase{
     }
 
     @Override
+    public Collection<Transaction> getTransactions(@NonNull AppAccount account) throws DataDoesNotExistException {
+        if (transactions.containsKey(account)) {
+            return transactions.get(account);
+        }
+
+        throw new DataDoesNotExistException("transactions");
+    }
+
+    @Override
     public void addAccount(@NonNull AppAccount newAccount) throws DataExistsException {
         if (!accounts.add(newAccount)) {
             throw new DataExistsException("accounts");
+        }
+    }
+
+    @Override
+    public void addTransaction(@NonNull AppAccount owner, @NonNull Transaction newTransaction)
+            throws DataExistsException, DataDoesNotExistException {
+        if (!transactions.containsKey(owner)) {
+            throw new DataDoesNotExistException("accounts");
+        }
+        if (!transactions.get(owner).add(newTransaction)){
+            throw new DataExistsException("transactions");
         }
     }
 
