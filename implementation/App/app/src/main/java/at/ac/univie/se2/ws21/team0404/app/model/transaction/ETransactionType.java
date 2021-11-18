ee164e7dd80129f40ad692789f2f17d4cf6ab85d
Michal Robert Zak <a11922222@unet.univie.ac.at>
2021-11-18 15:32:43 +0100

    added transaction type (income/expense) to transaction

---
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/model/transaction/ETransactionType.java
@@ -0,0 +1,7 @@
+package at.ac.univie.se2.ws21.team0404.app.model.transaction;
+
+// TODO: where will we track whether a transaction is income/expense? We can either assign this
+//  enum to transaction directly, or we can distinguish categories.
+public enum ETransactionType {
+    INCOME, EXPENSE
+}
