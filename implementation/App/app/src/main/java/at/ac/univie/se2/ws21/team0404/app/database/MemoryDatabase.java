michalrzak/UNIVIE_SE2_Project

commit c654370272c7cef4cf18e5fcd09d58f0ba91c9bb

Edit Category: update DB map & detect duplicate names

implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/MemoryDatabase.java
@@ -112,12 +112,16 @@ public class MemoryDatabase implements IDatabase {
 
   @Override
   public void updateCategory(@NonNull String categoryName, @NonNull Category newCategory)
-      throws DataDoesNotExistException {
+          throws DataDoesNotExistException, DataExistsException {
     if (!categories.containsKey(categoryName)) {
       throw new DataDoesNotExistException("categories");
     }
+    if (categories.containsKey(newCategory.getName())) {
+      throw new DataExistsException("categories");
+    }
 
-    categories.replace(categoryName, newCategory);
+    categories.remove(categoryName);
+    categories.put(newCategory.getName(), newCategory);
   }
 
   @Override

implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/IDatabase.java
@@ -40,7 +40,7 @@ public interface IDatabase {
   // public void updateAccount(AppAccount oldAccount, AppAccount newAccount) throws DataDoesNotExistException;
   void addCategory(Category newCategory) throws DataExistsException;
 
-  void updateCategory(String categoryName, Category newCategory) throws DataDoesNotExistException;
+  void updateCategory(String categoryName, Category newCategory) throws DataDoesNotExistException, DataExistsException;
 
   void updateTransaction(AppAccount owner, int oldId,
       Transaction updatedTransaction) throws DataDoesNotExistException;

implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/Repository.java
@@ -94,7 +94,7 @@ public class Repository {
   }
 
   public void updateCategory(@NonNull String categoryName, @NonNull Category category)
-      throws DataDoesNotExistException {
+          throws DataDoesNotExistException, DataExistsException {
     databaseStrategy.updateCategory(categoryName, category);
     reloadCategories();
   }

commit d1b58845cd46319b9e80493df75a316d2c89a3be

Bugfix: Delete Category did not work. Bug introduced with commit c6543702

implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/database/MemoryDatabase.java
@@ -117,7 +117,8 @@ public class MemoryDatabase implements IDatabase {
     if (!categories.containsKey(categoryName)) {
       throw new DataDoesNotExistException("categories");
     }
-    if (categories.containsKey(newCategory.getName())) {
+    // Check if renamed category already exists
+    if (!categoryName.equals(newCategory.getName()) && categories.containsKey(newCategory.getName())) {
       throw new DataExistsException("categories");
     }
 