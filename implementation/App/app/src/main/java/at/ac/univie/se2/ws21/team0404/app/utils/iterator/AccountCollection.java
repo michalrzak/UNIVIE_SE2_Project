ecc773166b417d5fab41f149e96e8a0855cbda9e
Rumen Angelov <a11911470@unet.univie.ac.at>
2022-01-15 21:03:15 +0100

    removed unnecessary files

--- implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/utils/iterator/AccountCollection.java
+++ implementation/App/app/src/main/java/at/ac/univie/se2/ws21/team0404/app/utils/iterator/AccountCollection.java
@@ -1,24 +1,15 @@
 package at.ac.univie.se2.ws21.team0404.app.utils.iterator;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import at.ac.univie.se2.ws21.team0404.app.model.account.AppAccount;
 
 public class AccountCollection implements ICollection<AppAccount> {
 
-    private List<AppAccount> accounts = new ArrayList<>();
-
-    public AccountCollection() {
-
-    }
+    private final List<AppAccount> accounts;
 
     public AccountCollection(List<AppAccount> accounts) {
         this.accounts = accounts;
-    }
-
-    public void addAccounts(List<AppAccount> accounts) {
-        this.accounts.addAll(accounts);
     }
 
     @Override
